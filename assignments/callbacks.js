943988c97c35942d0491b35adb4ab2a316ee0e60
chris pelayo <onetrckchris@gmail.com>
2019-05-07 13:32:30 -0700

    completed all but the stretch goal in callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'Yo-Yo', 'Gum', 'Notebook'];
 
 /* 
 
@@ -18,8 +18,8 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   }
 
   // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
+  firstItem(items, (first) => {
+    console.log(first);
   });
 
 */
@@ -27,24 +27,41 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+// getLength(items, console.log);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+// last(items, console.log);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+// sumNums(6, 7, console.log);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+// multiplyNums(5, 5, console.log)
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  list.forEach((listItem) => {
+    if (listItem === item) {
+      return cb(true);
+    } else {
+      cb(false);
+    }
+  })
 }
+// contains('Pencil', items, console.log)
 
 /* STRETCH PROBLEM */
 
@@ -53,3 +70,5 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+
+
